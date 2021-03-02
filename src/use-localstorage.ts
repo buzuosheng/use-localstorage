922ec93c4295168575b484f93f8dae74ef642505
wangqi <1015350043@qq.com>
2021-03-02 23:08:49 +0800

    1

--- src/use-localstorage.ts
+++ src/use-localstorage.ts
@@ -6,21 +6,26 @@ export default function useLocalStorage<T>(
   expireTime: number
 ): [T, Dispatch<T>] {
   const item = JSON.parse(window.localStorage.getItem(key) || '{}');
-  const [value, setValue] = useState<T>(item || initialValue);
+  const [value, setValue] = useState<T>(
+    window.localStorage.getItem(key) ? item : initialValue
+  );
 
   const setItem = (newValue: T) => {
     setValue(newValue);
     window.localStorage.setItem(key, JSON.stringify(newValue));
     window.localStorage.setItem(
       key + ':expireTime',
-      new Date().getTime().toString() + expireTime
+      JSON.stringify(Date.now() + expireTime)
     );
   };
 
   useEffect(() => {
     const time = window.localStorage.getItem(key + ':expireTime');
-    let isExpire = new Date().getTime() > Number(time);
-    if (isExpire) window.localStorage.removeItem(key);
+    let isExpire = Date.now() > Number(time);
+    if (isExpire) {
+      window.localStorage.removeItem(key);
+      window.localStorage.removeItem(key + ':expireTime');
+    }
     const newValue = window.localStorage.getItem(key);
     if (newValue && value !== JSON.parse(newValue)) {
       setValue(JSON.parse(newValue));
